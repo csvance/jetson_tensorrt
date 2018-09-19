872d037b34129488fcd7b589b0d3ace47a4261ad
Carroll Vance <vancecs@gmail.com>
2018-09-19 09:25:01 -0500

    Some initial work on DIGITS detection node

--- src/nodes/digits_detect.cpp
+++ src/nodes/digits_detect.cpp
@@ -26,9 +26,45 @@
 
  #include "digits_detect.h"
 
+ #include "DigitsDetector.h"
  #include "ros/ros.h"
+
+ DIGITSDetector engine;
+
+ void imageCallback(const sensor_msgs::Image::ConstPtr& msg)
+{
+
+}
+
 
  int main(int argc, char **argv)
  {
+
+   ros::init(argc, argv, "digits_detect");
+   ros::NodeHandle n;
+
+   std::string model_path = n.getParam("model_path");
+   std::string weights_path = n.getParam("weights_path");
+
+   std::string cache_path;
+   n.param("cache_path", cache_path, "detection.tensorcache");
+
+   int image_depth, image_width, image_height;
+   n.param("image_depth", &image_depth, 1);
+   n.param("image_width", &image_width, 1024);
+   n.param("image_height", &image_height, 512);
+
+   ROS_INFO("Loading nVidia DIGITS model...");
+   engine = DIGITSDetector(model_path, weights_path, cache_path,
+       image_depth, image_width, image_height, num_classes);
+
+   std::string image_subscribe_topic;
+   n.param("image_subscribe_topic", image_subscribe_topic, "camera");
+
+   ros::Subscriber image_sub = n.subscribe<sensor_msgs::Image>(image_subscribe_topic, 5, imageCallback);
+   ros::Publisher region_pub = n.advertise<jetson_tensorrt_msgs::CategorizedRegionsOfInterest>("detections", 5);
+
+   ros::spin();
+
    return 0;
  }
