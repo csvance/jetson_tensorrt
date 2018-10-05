4e315490b8f0263ac27bd3bb1f92d26551a7ae5b
Carroll Vance <vancecs@gmail.com>
2018-10-05 14:57:19 -0500

    Update docs

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['detect',['detect',['../classjetson__tensorrt_1_1_d_i_g_i_t_s_detector.html#a0792ee6a33be342f390d77f889ea4cee',1,'jetson_tensorrt::DIGITSDetector']]],
-  ['digitsclassifier',['DIGITSClassifier',['../classjetson__tensorrt_1_1_d_i_g_i_t_s_classifier.html#a33426eb6c0add60492c4d92ea0c7e631',1,'jetson_tensorrt::DIGITSClassifier']]],
-  ['digitsdetector',['DIGITSDetector',['../classjetson__tensorrt_1_1_d_i_g_i_t_s_detector.html#a578042a61daa60fc22c534a3e533bf8f',1,'jetson_tensorrt::DIGITSDetector']]]
+  ['digitsclassifier',['DIGITSClassifier',['../classjetson__tensorrt_1_1_d_i_g_i_t_s_classifier.html#a18db7cfb9dccb6acd70fe93865c29098',1,'jetson_tensorrt::DIGITSClassifier']]],
+  ['digitsdetector',['DIGITSDetector',['../classjetson__tensorrt_1_1_d_i_g_i_t_s_detector.html#aa6dfdedc5f11df0827ce18c2a385a4eb',1,'jetson_tensorrt::DIGITSDetector']]]
 ];
